f1d3546e1259254b32c126b87e7efcbc02448d6e
cuteestelle1004 <estherumejesi2@gmail.com>
2025-09-16 14:12:03 +0100

    i made deletions and updated

--- models.py
+++ models.py
@@ -1,73 +1,116 @@
-# # from django.db import models
-# # # A WEBSITE WHERE USERS CAN ORDER FOOD
-# # class Food:
-# #     price
-# #     description
-# #     images
-# #     name
-# # class Car:
-# #     my_car = Car()
-# #      wheels = 4 
-# #     def __init__(self, make, model):
-# #         self.make = make  # Instance attribute
-# #         self.model = model  # Instance attribute
-# class Car:
-#     def __init__(self, make, model):
-#         self.make = make
-#         self.model = model
-#         self.get_wheels =.get_wheels(cls)| (() -> get_wheels )
+# # # # from django.db import models
+# # # # # A WEBSITE WHERE USERS CAN ORDER FOOD
+# # # # class Food:
+# # # #     price
+# # # #     description
+# # # #     images
+# # # #     name
+# # # # class Car:
+# # # #     my_car = Car()
+# # # #      wheels = 4 
+# # # #     def __init__(self, make, model):
+# # # #         self.make = make  # Instance attribute
+# # # #         self.model = model  # Instance attribute
+# # # class Car:
+# # #     def __init__(self, make, model):
+# # #         self.make = make
+# # #         self.model = model
+# # #         self.get_wheels =.get_wheels(cls)| (() -> get_wheels )
 
 
-#     def display_info(self):
-#         print(f"Car make: {self.make}, Model: {self.model}")
-#         class Car:
-#          wheels = 4  # Class attribute
+# # #     def display_info(self):
+# # #         print(f"Car make: {self.make}, Model: {self.model}")
+# # #         class Car:
+# # #          wheels = 4  # Class attribute
 
-#     def get_wheels(cls):
-#         return cls.wheels
+# # #     def get_wheels(cls):
+# # #         return cls.wheels
     
-# class Car:
+# # # class Car:
 
-#     def honk():
-#         print("Honk! Honk!")
-#         my_car = Car("Toyota", "Camry")
-#         my_car.display_info()  # Output: Car make: Toyota, Model: Camry
-
-
-# print(Car.get_wheels())  # Output: 4
-# Car.honk()  # Output: Honk! Honk!
-
-class Car:
-    wheels = 4  # Class attribute
-
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
-
-    def display_info(self):
-        print(f"Car make: {self.make}, Model: {self.model}")
-
-    @classmethod
-    def get_wheels(cls):
-        return cls.wheels
-
-    @staticmethod
-    def honk():
-        print("Honk! Honk!")
+# # #     def honk():
+# # #         print("Honk! Honk!")
+# # #         my_car = Car("Toyota", "Camry")
+# # #         my_car.display_info()  # Output: Car make: Toyota, Model: Camry
 
 
-# Create an instance of Car
-my_car = Car("Toyota", "Camry")
+# # # print(Car.get_wheels())  # Output: 4
+# # # Car.honk()  # Output: Honk! Honk!
 
-# Display car info
-my_car.display_info()  # Output: Car make: Toyota, Model: Camry
+# # class Car:
+# #     wheels = 4  # Class attribute
 
-# Call class method
-print(Car.get_wheels())  # Output: 4
+# #     def __init__(self, make, model):
+# #         self.make = make
+# #         self.model = model
 
-# Call static method
-Car.honk()  # Output: Honk! Honk!
-Car.__prepare__()
+# #     def display_info(self):
+# #         print(f"Car make: {self.make}, Model: {self.model}")
+
+# #     @classmethod
+# #     def get_wheels(cls):
+# #         return cls.wheels
+
+# #     @staticmethod
+# #     def honk():
+# #         print("Honk! Honk!")
+
+
+# # # # Create an instance of Car
+# # # my_car = Car("Toyota", "Camry")
+
+# # # # Display car info
+# # # my_car.display_info()  # Output: Car make: Toyota, Model: Camry
+
+# # # # Call class method
+# # # print(Car.get_wheels())  # Output: 4
+
+# # # # Call static method
+# # # Car.honk()  # Output: Honk! Honk!
+# # # Car.__prepare__()
+
+
+# # class Car:
+# #     def __init__(self, color):
+# #         self.__color = color
+
+# #     def get_color(self):
+# #         return self.__color
+
+# #     def set_color(self, color):
+# #         self.__color = color
+
+# class Duck:
+#     def quack(self):
+#         print("Quack!")
+
+# class Dog:
+#     def quack(self):
+#         print("Bark like a quack!")
+
+# def make_it_quack(animal):
+#     animal.quack()
+
+# make_it_quack(Duck())  # Output: Quack!
+# make_it_quack(Dog())   # Output: Bark like a quack!
+
+student = {"name": "Alice", "age": 21, "grade": "A"}
+print(student["name"])  # Output: Alice
+
+print(student["age"] == 22)  # Updates age
+student["major"] = "Biology"  # Adds a new key-value pair
+# print(student["major"])
+
+print(student.pop("grade"))  # Removes "grade" key
+
+print(student.popitem())
+
+student.clear()
+"age" in student  # Output: True
+
+
+
+
 
 
 
